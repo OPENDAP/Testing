a866381414faf98484f228e84722e316d4bc7199
Samuel Lloyd <slloyd@opendap.org>
2022-06-01 10:26:58 -0600

    ascii and dap scripts modified

--- scripts/remote/dap/dap_aggFile_100x100_10-100_test.sh
+++ scripts/remote/dap/dap_aggFile_100x100_10-100_test.sh
@@ -1,132 +1,150 @@
 #!/bin/sh
 #
 
+SERVER_ENDPOINT="http://ngap-west.opendap.org:8080/opendap/"
+DATASET_PATH="agg/ghrsst_mur_agg_100time.dmrpp.dap"
+FULL_URL="${SERVER_ENDPOINT}${DATASET_PATH}"
+
+COOKIES_FILE="~/ursCookies"
+CURL_CMD="curl -n -c ${COOKIES_FILE} -b ${COOKIES_FILE} -L"
+RESULTS_FILE="dap_100x100_10-100_results.txt"
+
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:9][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:9]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:9][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:9]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 10.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 10 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 10 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:19][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:19]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:19][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:19]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 20.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 20 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 20 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:29][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:29]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:29][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:29]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 30.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 30 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 30 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:39][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:39]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:39][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:39]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 40.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 40 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 40 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:49][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:49]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:49][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:49]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 50.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 50 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 50 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:59][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:59]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:59][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:59]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 60.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 60 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 60 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:69][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:69]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:69][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:69]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 70.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 70 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 70 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:79][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:79]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:79][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:79]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 80.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 80 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 80 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:89][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:89]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:89][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:89]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 90.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 90 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 90 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:99][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:99]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:99][5000:5099][5000:5099];/lat[5000:5099];/lon[5000:5099];/time[0:1:99]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 100.$i ==========";
 done; 
-echo "" >> dap_100x100_10-100_results.txt;
-echo "100x100 - time 100 ..." >> dap_100x100_10-100_results.txt;
-grep real result.txt | cat >> dap_100x100_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "100x100 - time 100 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
--- scripts/remote/dap/dap_aggFile_500x500_10-100_test.sh
+++ scripts/remote/dap/dap_aggFile_500x500_10-100_test.sh
@@ -1,132 +1,150 @@
 #!/bin/sh
 #
 
+SERVER_ENDPOINT="http://ngap-west.opendap.org:8080/opendap/"
+DATASET_PATH="agg/ghrsst_mur_agg_100time.dmrpp.dap"
+FULL_URL="${SERVER_ENDPOINT}${DATASET_PATH}"
+
+COOKIES_FILE="~/ursCookies"
+CURL_CMD="curl -n -c ${COOKIES_FILE} -b ${COOKIES_FILE} -L"
+RESULTS_FILE="dap_500x500_10-100_results.txt"
+
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:9][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:9]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:9][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:9]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 10.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 10 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 10 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:19][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:19]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:19][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:19]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 20.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 20 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 20 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:29][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:29]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:29][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:29]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 30.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 30 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 30 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:39][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:39]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:39][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:39]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 40.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 40 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 40 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:49][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:49]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:49][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:49]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 50.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 50 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 50 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:59][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:59]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:59][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:59]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 60.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 60 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 60 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:69][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:69]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:69][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:69]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 70.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 70 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 70 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:79][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:79]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:79][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:79]" "${FULL_URL}";
     } 2>> result.txt; 
     echo ""; echo "========== 80.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 80 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 80 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:89][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:89]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:89][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:89]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 90.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 90 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 90 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
 
 for i in {1..10}; 
-    do { 
-        time -p curl -n -c ursCookies -b ursCookies -L --data-urlencode "dap4.ce=/analysed_sst[0:1:99][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:99]" "http://localhost:8080/opendap_server/data/agg/ghrsst_mur_agg_100time.dmrpp.dap"; 
+    do {
+        rm ${COOKIES_FILE};
+        time -p ${CURL_CMD} --data-urlencode "dap4.ce=/analysed_sst[0:1:99][5000:5499][5000:5499];/lat[5000:5499];/lon[5000:5499];/time[0:1:99]" "${FULL_URL}";
     } 2>> result.txt;  
     echo ""; echo "========== 100.$i ==========";
 done; 
-echo "" >> dap_500x500_10-100_results.txt;
-echo "500x500 - time 100 ..." >> dap_500x500_10-100_results.txt;
-grep real result.txt | cat >> dap_500x500_10-100_results.txt; 
+echo "" >> ${RESULTS_FILE};
+echo "500x500 - time 100 ..." >> ${RESULTS_FILE};
+grep real result.txt | cat >> ${RESULTS_FILE};
 rm result.txt;
 
 echo =========================================================================================;
