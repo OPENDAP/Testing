377509216e603b0173ee3693a2d4a73a7446a598
Samuel Lloyd <slloyd@opendap.org>
2022-06-21 10:57:27 -0600

    Revert "DAP file normalization"
    This reverts commit 049a7804403b361ffbdf14e09b05915ccbbc7761.

---
+++ scripts/remote/dap/dap_aggFile_500x500_10-100_test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+
+# Variables to be changed
+# /////////////////////////////////////////
+DAP_REQUEST="dap" # return type
+REGION_SIZE="5000:5499" # region size for curl cmd
+OUTPUT_SIZE="500x500" # human readable size
+# /////////////////////////////////////////
+
+SERVER_ENDPOINT="http://ngap-west.opendap.org:8080/opendap/"
+DATASET_PATH="agg/ghrsst_mur_agg_100time.dmrpp"
+DATASET_URL="${SERVER_ENDPOINT}${DATASET_PATH}"
+
+FULL_URL="${DATASET_URL}.${DAP_REQUEST}"
+echo "FULL_URL: ${FULL_URL}"
+
+10T_CE="dap4.ce=/analysed_sst[0:1:9][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:9]"
+20T_CE="dap4.ce=/analysed_sst[0:1:19][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:19]"
+30T_CE="dap4.ce=/analysed_sst[0:1:29][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:29]"
+40T_CE="dap4.ce=/analysed_sst[0:1:39][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:39]"
+50T_CE="dap4.ce=/analysed_sst[0:1:49][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:49]"
+60T_CE="dap4.ce=/analysed_sst[0:1:59][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:59]"
+70T_CE="dap4.ce=/analysed_sst[0:1:69][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:69]"
+80T_CE="dap4.ce=/analysed_sst[0:1:79][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:79]"
+90T_CE="dap4.ce=/analysed_sst[0:1:89][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:89]"
+100T_CE="dap4.ce=/analysed_sst[0:1:99][${REGION_SIZE}][${REGION_SIZE}];/lat[${REGION_SIZE}];/lon[${REGION_SIZE}];/time[0:1:99]"
+
+CE_LIST="${10T_CE} ${20T_CE} ${30T_CE} ${40T_CE} ${50T_CE} ${60T_CE} ${70T_CE} ${80T_CE} ${90T_CE} ${100T_CE}"
+
+COOKIES_FILE="/home/centos/ursCookies"
+CURL_CMD="curl -n -c ${COOKIES_FILE} -b ${COOKIES_FILE} -L"
+RESULTS_FILE="dap_${OUTPUT_SIZE}_10-100_results.txt"
+
+TIME=10
+
+for ce in ${CE_LIST}
+    do {
+        echo =========================================================================================;
+        echo ${FULL_URL}?${ce};
+        CE=$(echo "${ce}" | sed -e "s/\[/%5B/g" -e "s/\]/%5D/g")
+        REQUEST_URL="${FULL_URL}?${CE}"
+
+        for i in {1..10};
+            do {
+                echo ""; echo "========== ${TIME}.${i} ==========";
+                rm ${COOKIES_FILE};
+                time -p ${CURL_CMD} "${REQUEST_URL}";
+            } 2>> results.txt;
+            echo ""; echo "==================================";
+        done;
+        echo "" | tee -a ${RESULTS_FILE};
+        echo "${OUTPUT_SIZE} - time ${TIME} ..." | tee -a ${RESULTS_FILE};
+        grep real results.txt | tee -a ${RESULTS_FILE};
+        rm results.txt;
+    }
+    ((TIME=TIME+10))
+    done;
+echo =========================================================================================;
