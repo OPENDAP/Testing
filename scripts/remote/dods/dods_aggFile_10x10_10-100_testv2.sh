1d7882b4655484feaba4e27d123d2c4fa6257ba9
Samuel Lloyd <slloyd@opendap.org>
2022-06-21 12:32:14 -0600

    dods test script normalization

--- scripts/remote/dods/dods_aggFile_10x10_10-100_testv2.sh
+++ scripts/remote/dods/dods_aggFile_10x10_10-100_testv2.sh
@@ -4,33 +4,34 @@
 SERVER_ENDPOINT="http://ngap-west.opendap.org:8080/opendap/"
 DATASET_PATH="agg/ghrsst_mur_agg_100time.dmrpp"
 DATASET_URL="${SERVER_ENDPOINT}${DATASET_PATH}"
-DAP_REQUEST="dods"
+REQUEST_TYPE="dods"
 
-FULL_URL="${DATASET_URL}.${DAP_REQUEST}"
+FULL_URL="${DATASET_URL}.${REQUEST_TYPE}"
 echo "FULL_URL: ${FULL_URL}"
 
-REGION_SIZE="5000:5009"
+REGION_SIZE="5000:5009" # region size for curl cmd
+OUTPUT_SIZE="10x10" # human readable size
 
-DODS_10T_CE="analysed_sst[0:1:9][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:9]"
-DODS_20T_CE="analysed_sst[0:1:19][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:19]"
-DODS_30T_CE="analysed_sst[0:1:29][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:29]"
-DODS_40T_CE="analysed_sst[0:1:39][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:39]"
-DODS_50T_CE="analysed_sst[0:1:49][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:49]"
-DODS_60T_CE="analysed_sst[0:1:59][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:59]"
-DODS_70T_CE="analysed_sst[0:1:69][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:69]"
-DODS_80T_CE="analysed_sst[0:1:79][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:79]"
-DODS_90T_CE="analysed_sst[0:1:89][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:89]"
-DODS_100T_CE="analysed_sst[0:1:99][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:99]"
+10T_CE="analysed_sst[0:1:9][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:9]"
+20T_CE="analysed_sst[0:1:19][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:19]"
+30T_CE="analysed_sst[0:1:29][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:29]"
+40T_CE="analysed_sst[0:1:39][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:39]"
+50T_CE="analysed_sst[0:1:49][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:49]"
+60T_CE="analysed_sst[0:1:59][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:59]"
+70T_CE="analysed_sst[0:1:69][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:69]"
+80T_CE="analysed_sst[0:1:79][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:79]"
+90T_CE="analysed_sst[0:1:89][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:89]"
+100T_CE="analysed_sst[0:1:99][${REGION_SIZE}][${REGION_SIZE}],lat[${REGION_SIZE}],lon[${REGION_SIZE}],time[0:1:99]"
 
-DODS_CE_LIST="${DODS_10T_CE} ${DODS_20T_CE} ${DODS_30T_CE} ${DODS_40T_CE} ${DODS_50T_CE} ${DODS_60T_CE} ${DODS_70T_CE} ${DODS_80T_CE} ${DODS_90T_CE} ${DODS_100T_CE}"
+CE_LIST="${10T_CE} ${20T_CE} ${30T_CE} ${40T_CE} ${50T_CE} ${60T_CE} ${70T_CE} ${80T_CE} ${90T_CE} ${100T_CE}"
 
-COOKIES_FILE="~/ursCookies"
+COOKIES_FILE="/home/centos/ursCookies"
 CURL_CMD="curl -n -c ${COOKIES_FILE} -b ${COOKIES_FILE} -L"
-RESULTS_FILE="dods_10x10_10-100_results.txt"
+RESULTS_FILE="${REQUEST_TYPE}_${OUTPUT_SIZE}_10-100_results.txt"
 
 TIME=10
 
-for ce in ${DODS_CE_LIST}
+for ce in ${CE_LIST}
     do {
         echo =========================================================================================;
         echo ${FULL_URL}?${ce};
@@ -45,9 +46,9 @@ for ce in ${DODS_CE_LIST}
             } 2>> results.txt;
             echo ""; echo "==================================";
         done;
-        echo "" >> ${RESULTS_FILE};
-        echo "10x10 - time ${TIME} ..." >> ${RESULTS_FILE};
-        grep real results.txt | cat >> ${RESULTS_FILE};
+        echo "" | tee -a ${RESULTS_FILE};
+        echo "${OUTPUT_SIZE} - time ${TIME} ..." | tee -a ${RESULTS_FILE};
+        grep real results.txt | tee -a ${RESULTS_FILE};
         rm results.txt;
     }
     ((TIME=TIME+10))
